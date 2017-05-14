197a1adc286910d2ab194bbe3b563dd82e29505b
adrian <adriancarayol@gmail.com>
2017-05-14 16:37:47 +0200

    + Add queue to process

--- src/program.py
+++ src/program.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from src.algoritmos_ordenacion import Algoritmos
 
 
-def process_1(lista):
+def process_1(lista, q):
     """
     Funcion para el proceso 1
     :param lista: Lista de números a ordenar
@@ -14,12 +14,12 @@ def process_1(lista):
     print("Empezando: %s \n" % multiprocessing.current_process().name)
     Algoritmos.minimos_sucesivos(lista)
     end = time.time()
-    print(end - start)
-    print("Fin: %s \n" % multiprocessing.current_process().name)
+    q.put({'1': end - start})
+    print("Finalizado: %s en %f \n" % (multiprocessing.current_process().name, end - start))
     print(lista)
 
 
-def process_2(lista):
+def process_2(lista, q):
     """
     Funcion para el proceso 2
     :param lista: Lista de números a ordenar
@@ -28,8 +28,18 @@ def process_2(lista):
     start = time.time()
     Algoritmos.insertion_sort(lista)
     end = time.time()
-    print(end - start)
-    print("Fin: %s \n" % multiprocessing.current_process().name)
+    q.put({'2': end - start})
+    print("Finalizado: %s en %f \n" % (multiprocessing.current_process().name, end - start))
+    print(lista)
+
+
+def process_3(lista, q):
+    print("Empezando: %s \n" % multiprocessing.current_process().name)
+    start = time.time()
+    lista = sorted(lista)
+    end = time.time()
+    q.put({'3': end - start})
+    print("Finalizado: %s en %f \n" % (multiprocessing.current_process().name, end - start))
     print(lista)
 
 
@@ -45,19 +55,34 @@ def main():
         lista.append(random.randint(1, 1000))
 
     lista_2 = list(lista)  # Para algoritmo ordenacion por insercion
+    lista_3 = list(lista)  # Para algoritmo ordenacion por insercion
 
-    process_jobs = []
-    p1 = multiprocessing.Process(name='Minimos sucesivos', target=process_1, args=(lista,))
-    process_jobs.append(p1)
-    p2 = multiprocessing.Process(name='Ordenacion por seleccion (Algoritmo inestable)', target=process_2, args=(lista_2,))
-    process_jobs.append(p2)
+    queue = multiprocessing.Queue()  # Cola para los resultados
+
+    p1 = multiprocessing.Process(name='Minimos sucesivos', target=process_1, args=(lista, queue))
+
+    p2 = multiprocessing.Process(name='Ordenacion por seleccion (Algoritmo inestable)', target=process_2,
+                                 args=(lista_2, queue))
+
+    p3 = multiprocessing.Process(name='Ordenacion por Timsort', target=process_3,
+                                 args=(lista_3, queue))
 
     # Iniciar procesos
     p1.start()
     p2.start()
+    p3.start()
     # Espera hasta que el proceso haya terminado su trabajo
     p1.join()
     p2.join()
+    p3.join()
+
+    print(queue.get(1))
+    print(queue.get(2))
+    print(queue.get(3))
+    
+    plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'ro')
+    plt.axis([0, 6, 0, 20])
+    plt.show()
 
 
 if __name__ == "__main__":
