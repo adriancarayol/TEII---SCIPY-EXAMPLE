fdb59e0ecc8ff7edf7611f82234f1a5fa7ad4a9d
adrian <adriancarayol@gmail.com>
2017-05-14 20:44:00 +0200

    + Doc

--- src/program.py
+++ src/program.py
@@ -3,7 +3,7 @@ import time
 import multiprocessing
 import numpy as np
 import matplotlib.pyplot as plt
-from src.algoritmos_ordenacion import Algoritmos
+from algoritmos_ordenacion import Algoritmos
 
 
 def process_1(lista, q):
